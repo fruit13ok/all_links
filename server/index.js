ee210ab3aae9d3c5076ff39fd92ed0baf97f5c4d
fruit13ok <fruit13ok@gmail.com>
2020-05-24 22:03:48 -0700

    done scrape all links, it might take time

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const path = require('path');
 // 3rd party
 const express = require('express');
 const puppeteer = require('puppeteer');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const fetch = require("node-fetch");
 
@@ -16,6 +17,7 @@ const port = process.env.PORT || 8000;
 // MIDDLEWARE
 app.use(express.static(path.join(__dirname, '../public')));
 app.use('/css', express.static(__dirname + '../node_modules/bootstrap/dist/css'));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
@@ -33,6 +35,59 @@ app.listen(port, () => {
 
 // helper functions
 
+let checkUrl = async (url) => {
+    try {
+        const response = await fetch(url);
+        const status = await response.status;
+        return status.toString();
+    }catch (error) {
+        console.log(error);
+        return error;
+    }
+};
+
+let forLoop = async (resultArr) => {
+    let resultArray = [];
+    for (let i = 0; i < resultArr.length; i++) {
+        let curUrl = resultArr[i];
+        let curStatus = await checkUrl(curUrl);
+        resultArray.push({url: curUrl, status: curStatus});
+    }
+    return resultArray;
+}
+
+let scrape = async (targetPage) => {
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox', '--blink-settings=imagesEnabled=false']});
+    const page = await browser.newPage();
+    if(targetPage.startsWith('https://www.')){
+        console.log('https://www.');
+    }else if(targetPage.startsWith('http://www.')){
+        console.log('http://www.');
+        targetPage='https://www.'+targetPage.slice(11);
+    }else if(targetPage.startsWith('www.')){
+        console.log('www.');
+        targetPage='https://'+targetPage;
+    }else{
+        targetPage='https://www.'+targetPage;
+    }
+    // await page.goto(targetPage);
+    await page.goto(targetPage, {
+        waitUntil: 'networkidle2',
+        timeout: 30000
+    });
+
+    //either of these 3 ways return all links
+    const hrefs = await page.$$eval('a', as => as.map(a => a.href));
+    // const hrefs = await page.evaluate(() => {
+    //     return Array.from(document.getElementsByTagName('a'), a => a.href);
+    // });
+    // const hrefs = await Promise.all((await page.$$('a')).map(async a => {
+    //     return await (await a.getProperty('href')).jsonValue();
+    // }));
+    console.log('hrefs: ',hrefs.length, hrefs);
+    return hrefs;
+};
+
 // ROUTES
 // root
 app.get('/', function (req, res) {
@@ -42,8 +97,13 @@ app.get('/', function (req, res) {
 // post, get form data from frontend
 // return array of object with searchKey and count to frontend
 app.post('/api', async function (req, res) {
-    let curTargetPage = req.body.targetPage || "";
-    console.log("result", curTargetPage);
-    let rlist = [{key: curTargetPage, value: []}];
-    res.status(200).send(rlist);
+    req.setTimeout(0);
+    let targetPage = req.body.targetPage || "";
+    await scrape(targetPage)
+    .then((resultArr)=>{
+        forLoop(resultArr)
+        .then(resultArray => {
+            res.send(resultArray);
+        })
+    }).catch(() => {});    
 });
